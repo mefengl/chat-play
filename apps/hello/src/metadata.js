9939b9529ab82ff7be813f002d5e4d1d9682e055
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-03-21 16:13:18 +0800

    Initial commit

---
+++ apps/hello/src/metadata.js
@@ -0,0 +1,15 @@
+const metadata = `
+// ==UserScript==
+// @name chatgpt-hello
+// @namespace https://chatgptjs.org
+// @version 0.0.1
+// @description A template for a chatgpt userscript use chatgptjs
+// @author chatgptjs
+// @match https://chat.openai.com/*
+// @icon https://www.google.com/s2/favicons?sz=64&domain=openai.com
+// @grant none
+// @license MIT
+// ==/UserScript==
+`;
+
+export default metadata;
