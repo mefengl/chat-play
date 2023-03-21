68685d237794c8ad7f3f77e2e25ddb8e1b204219
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-03-21 17:55:51 +0800

    app(sensible): add send later feature

--- apps/sensible/src/metadata.js
+++ apps/sensible/src/metadata.js
@@ -2,12 +2,11 @@ const metadata = `
 // ==UserScript==
 // @name         chatgpt sensible
 // @namespace    https://github.com/mefengl
-// @version      0.5.8
+// @version      0.5.16
 // @description  sensible to me
 // @author       mefengl
 // @match        https://chat.openai.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
-// @require      https://raw.githubusercontent.com/chatgptjs/chatgpt.js/main/chatgpt.js
 // @grant        none
 // ==/UserScript==
 `;
