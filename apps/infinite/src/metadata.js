7ae3d64dda7ed0846a7c6b3952985f0371686403
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-03-21 16:26:39 +0800

    app(infinite): init

---
+++ apps/infinite/src/metadata.js
@@ -0,0 +1,15 @@
+const metadata = `
+// ==UserScript==
+// @name chatgpt-infinite
+// @namespace https://github.com/mefengl
+// @version 0.1.6
+// @description Infinite auto ask for chatgpt
+// @author mefengl
+// @match https://chat.openai.com/*
+// @icon https://www.google.com/s2/favicons?sz=64&domain=openai.com
+// @grant none
+// @license MIT
+// ==/UserScript==
+`;
+
+export default metadata;
