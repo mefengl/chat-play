bf8cd416d7791e8e0c42a5de41df7e5ae74dcbd0
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-03-21 16:55:29 +0800

    app(sensible): move sensible under this monorepo

---
+++ apps/sensible/README.md
@@ -0,0 +1,7 @@
+# chatgpt-sensible
+
+- [x] copy my message to clipboard when send it
+
+> 📝 GitHub: [chatgpt-sensible](https://github.com/mefengl/chatgpt-sensible)
+
+> 🍴 Greasy Fork: [chatgpt-sensible](https://greasyfork.org/scripts/460863)
---
+++ apps/sensible/src/metadata.js
@@ -0,0 +1,15 @@
+const metadata = `
+// ==UserScript==
+// @name         chatgpt sensible
+// @namespace    https://github.com/mefengl
+// @version      0.5.6
+// @description  sensible to me
+// @author       mefengl
+// @match        https://chat.openai.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
+// @require      https://raw.githubusercontent.com/chatgptjs/chatgpt.js/main/chatgpt.js
+// @grant        none
+// ==/UserScript==
+`;
+
+export default metadata;
