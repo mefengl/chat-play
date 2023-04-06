ce550c86addab16cbf566f3153c6403100f3c9aa
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-04-06 21:05:34 +0800

    chore(metadata.js): update version number to 0.3.5 in metadata.js file

--- apps/infinite/src/metadata.js
+++ apps/infinite/src/metadata.js
@@ -2,7 +2,7 @@ const metadata = `
 // ==UserScript==
 // @name chatgpt-infinite
 // @namespace https://github.com/mefengl
-// @version 0.3.3
+// @version 0.3.5
 // @description Infinite auto ask for chatgpt
 // @author mefengl
 // @match https://chat.openai.com/*
