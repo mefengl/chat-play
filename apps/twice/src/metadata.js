d6e02dd339b959c36ae58ece6e9105bfde26f7b4
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-04-08 15:22:03 +0800

    refactor(metadata.js): remove unused jquery dependency

--- apps/twice/src/metadata.js
+++ apps/twice/src/metadata.js
@@ -7,7 +7,6 @@ const metadata = `
 // @author       mefengl
 // @match        https://chat.openai.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
-// @require      https://cdn.staticfile.org/jquery/3.6.1/jquery.min.js
 // @grant        none
 // ==/UserScript==
 `;
