4df06dde47ebe3f291772ca66f7eb2033b3bc183
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-04-27 11:54:37 +0800

    Add reference links for possible DMCA processes in chatgpt index.ts file

--- packages/chatkit/src/chatgpt/index.ts
+++ packages/chatkit/src/chatgpt/index.ts
@@ -1,3 +1,27 @@
+/**
+
+Due to possible future code copyright disputes, 
+I will put some links below for reference in possible DMCA processes.
+
+At the same time, 
+any report on this code will be considered as made after viewing the content of the following links (not all links are included):
+
+https://github.com/mefengl/chatgpt-utils
+https://github.com/chatgptjs/chatgpt.js/pull/3
+https://github.com/chatgptjs/chatgpt.js/pull/5
+https://github.com/chatgptjs/chatgpt.js/pull/9
+https://github.com/chatgptjs/chatgpt.js/pull/11
+https://github.com/chatgptjs/chatgpt.js/pull/12
+https://github.com/chatgptjs/chatgpt.js/pull/13
+https://github.com/chatgptjs/chatgpt.js/pull/15
+https://github.com/chatgptjs/chatgpt.js/pull/18
+https://github.com/chatgptjs/chatgpt.js/pull/19
+https://github.com/chatgptjs/chatgpt.js/pull/20
+https://github.com/chatgptjs/chatgpt.js/pull/21
+
+This comment will be deleted after the report process is completed.
+ */
+
 export function getTextarea() {
   const form = document.querySelector('form');
   if (!form) return;
