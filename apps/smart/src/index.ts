4e820a0f85405d63fff880f053bf4d76ef9a7b43
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-05-08 19:41:50 +0800

    Add smart app files and dependenciesThe commit message summarizes the changes made in the commit. In this case, the commit adds new files to the smart app and includes dependencies for the app. The present tense is used to describe the changes as if they are happening at the moment of the commit

---
+++ apps/smart/src/index.ts
@@ -0,0 +1,19 @@
+import { onSend, getTextareaValue, setTextarea } from '@mefengl/chatkit/chatgpt';
+
+async function initialize() {
+  await new Promise(resolve => window.addEventListener('load', resolve));
+  await new Promise(resolve => setTimeout(resolve, 1000));
+}
+
+async function main() {
+  await initialize();
+  onSend(() => {
+    const textareaValue = getTextareaValue();
+    const smartPrompt = `Question:${textareaValue}\n\nAnswer: Let's work this out in a step by step way to be sure we have the right answer`;
+    setTextarea(smartPrompt);
+  });
+}
+
+(function () {
+  main();
+}());
---
+++ apps/smart/src/metadata.js
@@ -0,0 +1,15 @@
+const metadata = `
+// ==UserScript==
+// @name chat-smart
+// @namespace https://github.com/mefengl
+// @version ${process.env.npm_package_version}
+// @description Just an implementation of an Idea of "SmartGPT"
+// @author mefengl
+// @match https://chat.openai.com/*
+// @icon https://www.google.com/s2/favicons?sz=64&domain=openai.com
+// @grant none
+// @license MIT
+// ==/UserScript==
+`;
+
+export default metadata;
