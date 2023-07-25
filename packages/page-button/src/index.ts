0f940ad2ca809a1549d4b18b6d1009bf54399f1a
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-07-26 01:06:51 +0800

    refactor: Optimize displayHTML function

--- packages/page-button/src/index.ts
+++ packages/page-button/src/index.ts
@@ -1,8 +1,28 @@
 import Swal from 'sweetalert2';
 
-export function displayHTML(html: string[]) {
+export async function displayHTML(html: string[]) {
   let screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
   let swalWidth = screenWidth < 800 ? '100%' : '800px';
+
+  if (!document.head.querySelector('#LXGWStyle')) {
+    let link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/lxgw-wenkai-screen-webfont/1.7.0/style.css';
+    link.id = 'LXGWStyle';
+    document.head.appendChild(link);
+  }
+
+  if (!document.head.querySelector('#readModeStyle')) {
+    let style = document.createElement('style');
+    style.type = 'text/css';
+    style.id = 'readModeStyle';
+    style.innerHTML = `
+      .text-left { text-align: left !important; }
+      .scrollable { max-height: 80vh; overflow-y: auto; }
+      .swal-font { font-family: "LXGW WenKai Screen", sans-serif; }
+  `;
+    document.head.appendChild(style);
+  }
 
   Swal.fire({
     title: '',
@@ -13,7 +33,7 @@ export function displayHTML(html: string[]) {
     backdrop: 'rgba(128,128,128,0.4)',
     showConfirmButton: false,
     showClass: { popup: '', backdrop: '' },
-    customClass: { htmlContainer: 'text-left scrollable' },
+    customClass: { htmlContainer: 'text-left scrollable swal-font' },
     willClose: () => {
       const scrollable = document.querySelector('.scrollable');
       if (scrollable) {
@@ -27,15 +47,4 @@ export function displayHTML(html: string[]) {
       }
     }
   });
-
-  if (!document.head.querySelector('#readModeStyle')) {
-    let style = document.createElement('style');
-    style.type = 'text/css';
-    style.id = 'readModeStyle';
-    style.innerHTML = `
-      .text-left { text-align: left !important; }
-      .scrollable { max-height: 80vh; overflow-y: auto; }
-  `;
-    document.head.appendChild(style);
-  }
 }
