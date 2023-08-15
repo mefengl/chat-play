74a5a37f7c9353d53b281c4a7d4ed04c877a547c
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-08-15 20:13:42 +0800

    fix(page-button): the error handler not work for the last prompt
    chore: Delete DMCA references and comments, put them into a single file

--- packages/chatkit/src/chatgpt/index.ts
+++ packages/chatkit/src/chatgpt/index.ts
@@ -1,27 +1,3 @@
-/**
-
-Due to possible future code copyright disputes, 
-I will put some links below for reference in possible DMCA processes.
-
-At the same time, 
-any report on this code will be considered as made after viewing the content of the following links (not all links are included):
-
-https://github.com/mefengl/chatgpt-utils
-https://github.com/chatgptjs/chatgpt.js/pull/3
-https://github.com/chatgptjs/chatgpt.js/pull/5
-https://github.com/chatgptjs/chatgpt.js/pull/9
-https://github.com/chatgptjs/chatgpt.js/pull/11
-https://github.com/chatgptjs/chatgpt.js/pull/12
-https://github.com/chatgptjs/chatgpt.js/pull/13
-https://github.com/chatgptjs/chatgpt.js/pull/15
-https://github.com/chatgptjs/chatgpt.js/pull/18
-https://github.com/chatgptjs/chatgpt.js/pull/19
-https://github.com/chatgptjs/chatgpt.js/pull/20
-https://github.com/chatgptjs/chatgpt.js/pull/21
-
-This comment will be deleted after the report process is completed.
- */
-
 export function getNav(): HTMLElement | null {
   return document.querySelector('nav');
 }
@@ -185,7 +161,7 @@ export function setPromptListener(key: string = 'prompt_texts') {
         const isLong = prompt_texts.length > 60;
         if (prompt_texts.length > 0) {
           let firstTime = true;
-          while (prompt_texts.length > 0) {
+          while (true) {
             const waitTime = (isLong && !document.hasFocus()) ? 20 * 1000 : 2000;
             if (!firstTime) { await new Promise(resolve => setTimeout(resolve, waitTime)); }
             if (!firstTime && isGenerating()) {
@@ -200,6 +176,9 @@ export function setPromptListener(key: string = 'prompt_texts') {
               continue;
             }
             firstTime = false;
+            if (prompt_texts.length === 0) {
+              break;
+            }
             await send(prompt_texts.shift() || '');
           }
         }
