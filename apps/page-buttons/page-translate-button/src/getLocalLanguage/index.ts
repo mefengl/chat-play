48fcf323e03b7019749a8642607a13745ab5e110
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-08-22 16:31:12 +0800

    refactor: Remove unused getLocalLanguage function

--- apps/page-buttons/page-translate-button/src/getLocalLanguage/index.ts
+++
@@ -1,8 +0,0 @@
-function getLocalLanguage() {
-  const userLanguage = navigator.language;
-  const languageNames = new Intl.DisplayNames([userLanguage], { type: 'language' });
-  const readableLanguage = languageNames.of(userLanguage);
-  return readableLanguage;
-}
-
-export default getLocalLanguage;
